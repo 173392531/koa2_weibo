156c80a4562699c7bc20b7297c7430847ca238be
shq <sunheqi@jwzg.com>
2021-08-21 19:17:15 +0800

    路由功能

--- koa2-weibo/routes/users.js
+++ koa2-weibo/routes/users.js
@@ -10,4 +10,13 @@ router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
 })
 
+router.post('/login',async (ctx,next)=>{
+  const { userName,password } = ctx.request.body
+  // 在此处进行校验
+  ctx.body={
+    tag:100,
+    userName,
+    password,
+  }
+})
 module.exports = router
